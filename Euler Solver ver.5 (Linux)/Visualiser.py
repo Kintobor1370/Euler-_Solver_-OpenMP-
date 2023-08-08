6f170a47234c2ec56d7cab0e25950eaf764b752f
Leonid Inozemtsev <92368620+Kintobor1370@users.noreply.github.com>
2023-08-09 00:14:09 +0300

    Add files via upload

---
+++ Euler Solver ver.5 (Linux)/Visualiser.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import matplotlib.patches as mp
+import numpy as np
+from array import array
+
+file = open('Data/VisData.txt', 'r')
+ExperimentNumber = float(file.readline())
+title = file.readline()
+xLabel = file.readline()
+yLabel = file.readline()
+size = int(file.readline())
+
+x = array('d')
+y1 = array('d')
+y2 = array('d')
+eq = array('d')
+x = np.fromfile(file, 'd', size, '\n')
+y1 = np.fromfile(file, 'd', size, '\n')
+y2 = np.fromfile(file, 'd', size, '\n')
+eq = np.full(size, 200)
+
+plt.plot(x, y1, color='blue')
+plt.plot(x, y2, color='orange')
+plt.title(title)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.ylim(0, 350)
+
+if ExperimentNumber == 14.6:
+    plt.xlim(0, 0.2)
+    plt.ylim(0, 400)
+    plt.plot(x, eq, linestyle = '--', color = 'grey')
+
+plt.show()
+
+
+
+plt.plot(x, y1, color='blue')
+plt.plot(x, y2, color='orange')
+plt.title(title)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+
+patch1 = mp.Patch(color='blue', label='N')
+patch2 = mp.Patch(color='orange', label='Square root of C at cutoff distance')
+plt.legend(handles=[patch1, patch2])
+
+plt.xlim(0, x[size-1])
+plt.ylim(0, 350)
+if ExperimentNumber == 14.6:
+    plt.ylim(0, 400)
+    plt.plot(x, eq, linestyle = '--', color = 'grey')
+
+plt.show()
+
+
+
+er = [0] * size
+for i in range(size):
+    er[i] = abs(y1[i] - y2[i])
+    er[i] = er[i] / y2[i]
+    er[i] = er[i] * 100
+
+
+plt.plot(x, er, color='blue')
+plt.title(title)
+plt.xlabel(xLabel)
+plt.ylabel('Error (in %)')
+plt.xlim(0, x[size-1])
+plt.ylim(0, 10)
+
+print('\n============================PLOTS CREATED!=============================\n')
+plt.show()
